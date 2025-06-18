c4eb36318d88bb425de36c6eb8ce5a307cf26c4d
dyn1xx <dyn1x@protonmail.com>
2025-06-18 21:28:33 +0300

    initial commit

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,29 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┐
+     * │M1 │M1 │
+     * └───┴───┘
+
+
+     */
+    [0] = LAYOUT(
+        LCTL(MS_BTN1),   LCTL(MS_BTN1)
+    )
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        if (clockwise) {
+            tap_code(KC_VOLU);
+        } else {
+            tap_code(KC_VOLD);
+        }
+    }
+    return false;
+}
+
